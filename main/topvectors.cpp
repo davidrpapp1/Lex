e99d217b5df34444157f8e7b024fb1ae338724a6
Erik <erikpapp@external.mol.hu>
2022-11-08 19:02:26 +0000

    topvec_comments

--- main/topvectors.cpp
+++ main/topvectors.cpp
@@ -59,62 +59,56 @@ void printvector(std::vector <std::string> v){
 
 
 int main(){
-    //outvec("sense_and_sensibility.txt");
     //
     std::string file_contents;
     std::vector <std::string> out;
-    std::vector <std::string> realout;
 
-    file_contents = readFileIntoString("sense_and_sensibility.txt");
-        std::transform(file_contents.begin(), file_contents.end(),file_contents.begin(), ::toupper);
+    file_contents = readFileIntoString("sense_and_sensibility.txt"); //reads file to string as file_contents
+        std::transform(file_contents.begin(), file_contents.end(),file_contents.begin(), ::toupper); //makes all caps
 
-    out = tokenise(file_contents);
+    out = tokenise(file_contents); //tokenise file string
     //
 
-
-    int x = 15;
-    int rx = 14;
-    std::string samp = "qqqqqq";
-    std::vector <std::string> topvec(x, samp);
-    std::vector <std::string> badvec;
+    //topvec function:
+    int x = 15; //top x most used words
+    int rx = 14; //real x as 0 is included in functions
+    std::string samp = "qqqqqq"; //placeholder string which would not be used at all in text file
+    std::vector <std::string> topvec(x, samp); //fill topvec vector with placeholder
+    std::vector <std::string> badvec;  //list of words to words to ignore as disqualified
     badvec.push_back ("");
-    int botcount = 0;
+    int botcount = 0; //number of appearances of last word in topvec
 
 
-    for(int i=0;i<out.size();++i){
+    for(int i=0;i<out.size();++i){ //iterate through each word in file
 
-        if(std::count(badvec.begin(), badvec.end(), out[i]) > 0){}
-        else if(std::count(topvec.begin(), topvec.end(), out[i]) > 0){
-            badvec.push_back (out[i]);
+        if(std::count(badvec.begin(), badvec.end(), out[i]) > 0){} //check if already disqualified
+        else if(std::count(topvec.begin(), topvec.end(), out[i]) > 0){ //check if already in topvec
+            badvec.push_back (out[i]); //disqualify
         }
-        else if(std::count(out.begin(), out.end(), out[i]) < botcount){
-            badvec.push_back (out[i]);
+        else if(std::count(out.begin(), out.end(), out[i]) < botcount){ //check if qualifies at all
+            badvec.push_back (out[i]); //disqualify
         }
     
-        else{
+        else{ //if qualifies
             badvec.push_back (out[i]);
-            for(int t=0;t<x;t++){
-                if(std::count(out.begin(), out.end(), out[i]) > std::count(out.begin(), out.end(), topvec[t])){
-                    if(t == rx){
+            for(int t=0;t<x;t++){ //iterate through topvec from right to left
+                if(std::count(out.begin(), out.end(), out[i]) > std::count(out.begin(), out.end(), topvec[t])){ //check against
+                    if(t == rx){ //if at last topvec value, replace topvec with candidate
                         topvec[rx] = out[i];
                     }
                     else{
-                        for(int f=0;f<rx-t;f++){
+                        for(int f=0;f<rx-t;f++){ //if not at last topvec value, each topvec value until current is replaced by the one before
                             topvec[rx-f] = topvec[rx-f-1];
                         }
-                        topvec[t] = out[i];
+                        topvec[t] = out[i]; //appropriate value in topvec replaced by candidate
                     }
-                    break;
+                    break; //breaks loop once topvec updated
                 }
             }
-            botcount = std::count(out.begin(), out.end(), topvec[rx]);
+            botcount = std::count(out.begin(), out.end(), topvec[rx]); //new botcount value assigned
         }
     }
     printvector(topvec);
 
-    //for(int g=0;g<topvec.size();g++){
-    //    std::cout << topvec[g] << std::endl;
-    //}
-    //outvec("sense_and_sensibility.txt");
     return 0;
 }
